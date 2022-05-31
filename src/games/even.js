86ba5d8b089571fb1edb49ac0e8287efdfa21847
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-05-31 23:08:49 +0300

    redid the logic, add even.js

---
+++ src/games/even.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const correctAnswer = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let Answer;
+  for (let i = 1; i <= 3; i += 1) {
+    const number = Math.floor(Math.random() * 100);
+    console.log(`Question: ${number}`);
+    if (number % 2 === 0) Answer = 'yes';
+    else Answer = 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== Answer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${Answer}'.`);
+      return console.log(`Let's try again, ${userName}!`);
+    }
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default correctAnswer;
