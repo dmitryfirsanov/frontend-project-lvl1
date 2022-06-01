83154911daa522606ad53c458582562bfa5a958a
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-06-01 18:15:42 +0300

    made logic for the game

---
+++ src/logicGames.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+const logic = (condition, taskResponse) => {
+  const roundsMax = 3;
+
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(condition);
+
+  for (let i = 0; i < roundsMax; i += 1) {
+    const [question, answer] = taskResponse();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== answer) {
+      console.log(`'${userAnswer}' is wrong answer;(. Correct answer was '${answer}'`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default logic;
