4e647399b3a25be88f7e1e4c85a7103256241ba0
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-06-01 21:16:25 +0300

    add brain-prime and prime

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import randomNumber from '../randomNumber.js';
+import logic from '../logicGames.js';
+
+const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number <= 1) return false;
+  let answer = true;
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) answer = false;
+  }
+  return answer;
+};
+
+const taskResponse = () => {
+  const number = randomNumber();
+  const question = String(number);
+  const answer = (isPrime(number)) === true ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const runPrimeGame = () => logic(condition, taskResponse);
+
+export default runPrimeGame;
